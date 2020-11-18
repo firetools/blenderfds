2189dc63d6b680b7d5f461122c870c3a5e35eb2d
Emanuele Gissi <emanuele.gissi@gmail.com>
2020-11-18 16:23:50 +0100

    Fix make_release script

--- dev/make_release.py
+++ dev/make_release.py
@@ -1,4 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+# BlenderFDS, an open tool for the NIST Fire Dynamics Simulator
+# Copyright (C) 2013  Emanuele Gissi, http://www.blenderfds.org
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""!
+Automatic creation of blenderfds release zip.
+"""
 
 import shutil, tempfile
 
@@ -6,9 +26,11 @@ import shutil, tempfile
 ignore_patterns = (
     "__pycache__",
     "*.pyc",
-    ".git*",
+    ".git",
+    ".gitignore",
     ".vscode",
-    "*.blend1",
+    "startup.blend1",
+    "TODO.md",
     "dev",
     "docs",
     "verification",
