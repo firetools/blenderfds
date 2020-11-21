d3f98cb47217d3d230a8294fcceec8ee80c19f29
Emanuele Gissi <emanuele.gissi@gmail.com>
2020-11-21 11:10:44 +0100

    Fix directory creation

--- dev/make_release.py
+++ dev/make_release.py
@@ -39,13 +39,12 @@ output_filename = "blenderfds"
 
 # Make
 with tempfile.TemporaryDirectory() as tmpdirname:
-    blenderfds_dir = tmpdirname + "/blenderfds"
     shutil.copytree(
         "..",
-        blenderfds_dir,
+        tmpdirname + "/blenderfds",
         symlinks=False,
         ignore=shutil.ignore_patterns(*ignore_patterns),
     )
-    shutil.make_archive(output_filename, "zip", blenderfds_dir)
+    shutil.make_archive(output_filename, "zip", tmpdirname)
 
 print(f"Done: {output_filename}.zip")
