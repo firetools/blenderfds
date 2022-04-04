24029e21818f0b9e58c00fe01d2f4b66dbd705f1
Emanuele Gissi <emanuele.gissi@gmail.com>
2022-04-04 16:07:36 +0200

    Separate show/hide_fds_geometry, separate show_ui

--- bl/operators/show_fds_geometry.py
+++ bl/operators/show_fds_geometry.py
@@ -18,32 +18,19 @@ class OBJECT_OT_bf_show_fds_geometry(Operator):
     Show geometry of Object as exported to FDS.
     """
 
-    bl_label = "FDS Geometry"
+    bl_label = "Show FDS Geometry"
     bl_idname = "object.bf_show_fds_geometry"
     bl_description = "Show/Hide geometry as exported to FDS"
 
     @classmethod
     def poll(cls, context):
-        return context.active_object
+        ob = context.object
+        return ob and not ob.bf_is_tmp and not ob.bf_has_tmp
 
     def execute(self, context):
         w = context.window_manager.windows[0]
         w.cursor_modal_set("WAIT")
-        ob = context.active_object
-
-        # Hide tmp
-        if ob.bf_is_tmp or ob.bf_has_tmp:
-            utils.geometry.rm_tmp_objects()
-            try:
-                context.view_layer.objects.active = ob
-            except ReferenceError:
-                try:
-                    context.view_layer.objects.active = context.selected_objects[0]
-                except IndexError:
-                    pass
-            w.cursor_modal_restore()
-            self.report({"INFO"}, "Temporary geometry hidden")
-            return {"FINISHED"}
+        ob = context.object
 
         # Check
         bf_namelist = ob.bf_namelist
@@ -88,8 +75,37 @@ class OBJECT_OT_bf_show_fds_geometry(Operator):
             w.cursor_modal_restore()
 
 
+class SCENE_OT_bf_hide_tmp_geometry(Operator):
+    """!
+    Hide all temporary geometry.
+    """
+
+    bl_label = "Hide FDS Geometry"
+    bl_idname = "scene.bf_hide_fds_geometry"
+    bl_description = "Hide all generated temporary geometry"
+
+    def execute(self, context):
+        w = context.window_manager.windows[0]
+        w.cursor_modal_set("WAIT")
+
+        ob = context.object
+        utils.geometry.rm_tmp_objects()
+        try:
+            context.view_layer.objects.active = ob
+        except ReferenceError:
+            try:
+                context.view_layer.objects.active = context.selected_objects[0]
+            except IndexError:
+                pass
+
+        w.cursor_modal_restore()
+        self.report({"INFO"}, "Temporary geometry hidden")
+        return {"FINISHED"}
+
+
 bl_classes = [
     OBJECT_OT_bf_show_fds_geometry,
+    SCENE_OT_bf_hide_tmp_geometry,
 ]
 
 
