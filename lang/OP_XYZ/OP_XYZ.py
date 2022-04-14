d8f26b869959f6f69aeef22f05ac6850dbef4268
Emanuele Gissi <emanuele.gissi@gmail.com>
2022-04-14 17:24:13 +0200

    Fix previous fix of update functions

--- lang/OP_XYZ/OP_XYZ.py
+++ lang/OP_XYZ/OP_XYZ.py
@@ -11,8 +11,9 @@ log = logging.getLogger(__name__)
 
 
 def update_bf_xyz(ob, context):
-    # Remove tmp objects
-    utils.geometry.rm_tmp_objects()
+    utils.geometry.rm_geometric_cache(ob=ob)
+    if ob.bf_has_tmp:
+        utils.geometry.rm_tmp_objects()
     # Prevent double multiparam
     if ob.bf_xyz == "VERTICES" and ob.bf_xyz_export:
         if ob.bf_xb in ("VOXELS", "FACES", "PIXELS", "EDGES"):
