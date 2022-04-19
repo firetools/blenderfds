fafe6673031d36f15df61e8af107153b42499379
Emanuele Gissi <emanuele.gissi@gmail.com>
2022-04-19 16:54:22 +0200

    Remove support for older file versions

--- bl/handlers.py
+++ bl/handlers.py
@@ -19,47 +19,15 @@ def _load_post(self):
     # Beware: self is None
     # Check file format version
     bf_file_version = tuple(bpy.data.scenes[0].bf_file_version)
-    # log.debug(f"Load post, file version: <{bf_file_version}>")
-    if bf_file_version == (4, 0, 0):
-        for ma in bpy.data.materials:
-            ma.bf_namelist_cls = "MN_SURF"
-        for ob in bpy.data.objects:
-            try:
-                bf_xb = ob["bf_xb"]
-            except KeyError:
-                pass
-            else:
-                if bf_xb == 0 or ob.bf_xb == "":  # was "NONE" or bad
-                    ob.bf_xb = "BBOX"
-                    ob.bf_xb_export = False
-                else:
-                    ob.bf_xb_export = True
-            try:
-                bf_xyz = ob["bf_xyz"]
-            except KeyError:
-                pass
-            else:
-                if bf_xyz == 0 or ob.bf_xyz == "":  # was "NONE" or bad
-                    ob.bf_xyz = "VERTICES"
-                    ob.bf_xyz_export = False
-                else:
-                    ob.bf_xyz_export = True
-            try:
-                bf_pb = ob["bf_pb"]
-            except KeyError:
-                pass
-            else:
-                if bf_pb == 0 or ob.bf_pb == "":  # was "NONE" or bad
-                    ob.bf_pb = "PLANES"
-                    ob.bf_pb_export = False
-                else:
-                    ob.bf_pb_export = True
+
+    if bf_file_version < config.supported_file_version:
         bpy.ops.wm.bf_dialog(
             "INVOKE_DEFAULT",
             msg="Check your data!",
             description="This file was created with an old BlenderFDS version.",
             type="ERROR",
         )
+
     elif bf_file_version > config.supported_file_version:
         bpy.ops.wm.bf_dialog(
             "INVOKE_DEFAULT",
@@ -67,55 +35,6 @@ def _load_post(self):
             description="This file was created with a new BlenderFDS version.",
             type="ERROR",
         )
-    else:
-        # Fix some properties, due to a development error!
-        for ma in bpy.data.materials:
-            try:
-                ma["bf_backing"] = {0: 100, 1: 200, 2: 300}[ma["bf_backing"]]
-            except KeyError:
-                if ma.bf_backing == "":
-                    ma.bf_backing = "EXPOSED"
-        for ob in bpy.data.objects:
-            try:
-                ob["bf_xb"] = {
-                    0: 100,
-                    1: 200,
-                    2: 300,
-                    3: 400,
-                    4: 500,
-                }[ob["bf_xb"]]
-            except KeyError:
-                if ob.bf_xb == "":
-                    ob.bf_xb = "BBOX"
-            try:
-                ob["bf_xyz"] = {
-                    0: 100,
-                    1: 200,
-                }[ob["bf_xyz"]]
-            except KeyError:
-                if ob.bf_xyz == "":
-                    ob.bf_xyz = "CENTER"
-            try:
-                ob["bf_pb"] = {
-                    0: 100,
-                }[ob["bf_pb"]]
-            except KeyError:
-                if ob.bf_pb == "":
-                    ob.bf_pb = "PLANES"
-            try:
-                ob["bf_id_suffix"] = {
-                    0: 100,
-                    1: 200,
-                    2: 300,
-                    3: 400,
-                    4: 500,
-                    5: 600,
-                    6: 700,
-                    7: 800,
-                }[ob["bf_id_suffix"]]
-            except KeyError:
-                if ob.bf_id_suffix == "":
-                    ob.bf_id_suffix = "IDI"
 
     # Remove all caches and tmp objects, clean up to remove stale caches
     utils.geometry.rm_geometric_caches()
@@ -131,7 +50,6 @@ def _load_post(self):
         ma.use_fake_user = True
 
     # Set default object and material appearance
-    # TODO scene appearance?
     context = bpy.context
     for ob in bpy.data.objects:
         bf_namelist = ob.bf_namelist
@@ -169,7 +87,6 @@ def _depsgraph_update_post(scene):
             and ob.type in {"MESH", "CURVE", "SURFACE", "FONT", "META"}
             and (update.is_updated_geometry or update.is_updated_transform)
         ):
-            # log.debug(f"Remove <{ob.name}> xbs cache")
             ob["ob_to_xbs_cache"] = None
 
 
@@ -185,6 +102,6 @@ def register():
 
 def unregister():
     log.debug("Unregistering handlers...")
-    # depsgraph_update_post.remove(_depsgraph_update_post)
+    # depsgraph_update_post.remove(_depsgraph_update_post)  # FIXME why?
     save_pre.remove(_save_pre)
     load_post.remove(_load_post)
