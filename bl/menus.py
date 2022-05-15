eb8953abe59e8c9940c4deabfd424330c36db04f
Emanuele Gissi <emanuele.gissi@gmail.com>
2022-05-15 21:09:31 +0200

    Add .fds to menus, as others

--- bl/menus.py
+++ bl/menus.py
@@ -6,12 +6,14 @@ import bpy
 
 
 def menu_func_import_FDS(self, context):
-    self.layout.operator("import_to_current_scene.fds", text="NIST FDS as Scene")
+    self.layout.operator("import_to_current_scene.fds", text="NIST FDS as Scene (.fds)")
 
 
 def menu_func_export_to_fds(self, context):
-    self.layout.operator("export_all_scene.fds", text="All Scenes as NIST FDS")
-    self.layout.operator("export_current_scene.fds", text="Current Scene as NIST FDS")
+    self.layout.operator("export_all_scene.fds", text="All Scenes as NIST FDS (.fds)")
+    self.layout.operator(
+        "export_current_scene.fds", text="Current Scene as NIST FDS (.fds)"
+    )
 
 
 def register():
