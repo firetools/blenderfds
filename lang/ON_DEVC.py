5bea1fa923244ba860895c78f052d6d72117a554
Emanuele Gissi <emanuele.gissi@gmail.com>
2022-06-05 21:39:56 +0200

    Add OP_SURF_ID to DEVC so it is managed

--- lang/ON_DEVC.py
+++ lang/ON_DEVC.py
@@ -7,6 +7,7 @@ from ..types import BFParam, BFNamelistOb
 from .bf_object import OP_namelist_cls, OP_ID, OP_FYI, OP_ID_suffix, OP_other
 from .OP_XB import OP_XB, OP_XB_voxel_size, OP_XB_center_voxels
 from .OP_XYZ import OP_XYZ
+from .OP_SURF_ID import OP_SURF_ID
 
 log = logging.getLogger(__name__)
 
@@ -47,6 +48,7 @@ class ON_DEVC(BFNamelistOb):
         OP_FYI,
         OP_DEVC_QUANTITY,
         OP_DEVC_PROP_ID,
+        OP_SURF_ID,
         OP_XB,
         OP_XB_voxel_size,
         OP_XB_center_voxels,
